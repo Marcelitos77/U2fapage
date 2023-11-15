a3480938b4c01b3cd6b312f23dcfe4d554e97e72
Marcelo Siqueira Oliveira <147945286+Marcelitos77@users.noreply.github.com>
2023-11-15 11:48:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    function typeWriter(text, i) {
+        if (i < text.length) {
+            spanElement.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(function () { typeWriter(text, i); }, 100); 
+        }
+    }
+
+    typeWriter(text, 0);
+});
